6ec26d13acea679ab9dc1b6f1a41715ff2f40591
Anthematics <jasonharder2@gmail.com>
2017-09-15 13:07:53 -0400

    Robot exercises finished - moving into TOdo list

--- test_robot.rb
+++ test_robot.rb
@@ -4,76 +4,86 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
-  def test_that_foreign_robot_neeing_repairs_sent_to_station_1
-    skip
-    # arrange
+	def test_that_foreign_robot_needing_repairs_sent_to_station_1
+		# arrange
+		robot = Robot.new
+		robot.needs_repairs = true
+		robot.foreign_model = true
+		# act
+		result = robot.station
+		# assert
+		assert_equal 1, result
 
-    # act
+	end
 
-    # assert
-  end
+	def test_that_vintage_robot_needing_repairs_sent_to_station_2
+		# arrange
+		robot =Robot.new
+		robot.needs_repairs = true
+		robot.vintage_model = true
 
-  def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
-    # arrange
+		# act
+		result = robot.station
+		# assert
+		assert_equal 2, result
 
-    # act
 
-    # assert
-  end
+	end
 
-  def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
-    # arrange
+	def test_that_standard_robot_needing_repairs_sent_to_station_3
+		# arrange
+		robot = Robot.new
+		robot.needs_repairs = true
+		# act
+		result = robot.station
+		# assert
+		assert_equal 3, result
+	end
 
-    # act
+	def test_that_robot_in_good_condition_sent_to_station_4
+		# arrange
+		robot = Robot.new
 
-    # assert
-  end
+		# act
+		result= robot.station
+		# assert
+		assert_equal 4, result
+	end
 
-  def test_that_robot_in_good_condition_sent_to_station_4
-    skip
-    # arrange
+	def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
+		skip
+		# arrange
 
-    # act
+		# act
 
-    # assert
-  end
+		# assert
+	end
 
-  def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
-    # arrange
+	def test_prioritize_tasks_with_todos_returns_max_todo_value
+		skip
+		# arrange
 
-    # act
+		# act
 
-    # assert
-  end
+		# assert
+	end
 
-  def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
-    # arrange
+	def test_workday_on_day_off_returns_false
+		skip
+		# arrange
 
-    # act
+		# act
 
-    # assert
-  end
+		# assert
+	end
 
-  def test_workday_on_day_off_returns_false
-    skip
-    # arrange
+	def test_workday_not_day_off_returns_true
+		skip
+		# arrange
 
-    # act
+		# act
 
-    # assert
-  end
-
-  def test_workday_not_day_off_returns_true
-    skip
-    # arrange
-
-    # act
-
-    # assert
-  end
+		# assert
+	end
 
 end
